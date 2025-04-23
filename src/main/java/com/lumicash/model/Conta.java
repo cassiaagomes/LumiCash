1a6fcc0676285307b6c201f116742e8d926f322a
Cassia Gomes <128179609+cassiaagomes@users.noreply.github.com>
2025-04-23 19:10:29 -0300

    feat: adiciona classe Conta com campos numero, descricao, tipo, diaFechamento e relação com correntista

---
+++ src/main/java/com/lumicash/model/Conta.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class Conta {
+    private Long id;
+    private String numero;
+    private String descricao;
+    private String tipo;
+    private Integer diaFechamento; // Dia de fechamento da conta if tipo == CARTAO
+
+    private Correntista correntista; 
+    private List<Transacao> transacoes; 
+}
