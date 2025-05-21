7b643bec8e96c133b35a1748cb86f9de2f6f2a32
Cassia Gomes <128179609+cassiaagomes@users.noreply.github.com>
2025-05-21 19:47:57 -0300

    [LP-8] - Refactor, Feat:
    Modificações
    
    Refactor: Alterações realizadas para o método de Salvar.
    Feat:  Implementação dos métodos de encontrarPorId e encontrarporEmail.

--- src/main/java/br/edu/ifpb/pweb2/lumicash/service/CorrentistaService.java
+++ src/main/java/br/edu/ifpb/pweb2/lumicash/service/CorrentistaService.java
@@ -2,45 +2,33 @@ package br.edu.ifpb.pweb2.lumicash.service;
 
 import java.util.List;
 import java.util.ArrayList;
-import br.edu.ifpb.pweb2.lumicash.entity.Correntista; 
+import br.edu.ifpb.pweb2.lumicash.entity.Correntista;
+import br.edu.ifpb.pweb2.lumicash.repository.CorrentistaRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CorrentistaService {
-    private List<Correntista> correntistas = new ArrayList<>();
-    private int proximoId = 1;
 
-    public void adicionarCorrentista(Correntista correntista) {
-        if (correntista.getNome() == null || correntista.getEmail() == null|| correntista.getSenha() == null) {
-            throw new IllegalArgumentException("Dados não podem ser vazios");
-        } else if (correntista.getNome().length() < 3) {
-            throw new IllegalArgumentException("Nome deve ter pelo menos 3 caracteres");
-        } else if (correntista.getEmail().length() < 5) {
-            throw new IllegalArgumentException("Email deve ter pelo menos 5 caracteres");
-        } else if (correntista.getSenha().length() < 6) {
-            throw new IllegalArgumentException("Senha deve ter pelo menos 6 caracteres");
-        } else if (correntista.getIsAdmin() == null) {
-            throw new IllegalArgumentException("Você deve informar se é admin ou não");
-        }
-        for (Correntista c : correntistas) {
-            if (c.getEmail().equals(correntista.getEmail())) {
-                throw new IllegalArgumentException("Email já cadastrado");
-            }
-        }
-        correntista.setId((long) proximoId);
-        correntistas.add(correntista);
-        proximoId++;
+    @Autowired
+    private  CorrentistaRepository repository;
+
+    public Correntista salvar(Correntista correntista) {
+        return repository.save(correntista);
     }
 
-    public List<Correntista> listarCorrentistas() {
-        return correntistas;
+    public Correntista encontrarPorId(Long id){
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("Correntista não encontrado"));
     }
 
-    public Correntista buscarPorId (int id) {
-        for (Correntista c: correntistas) {
-            if (c.getId() == id) {
-                return c;
-            }
-        }
-        return null;
+    public Correntista encontrarporEmail(String email)throws Exception{
+        return repository.findByEmail(email).orElseThrow(() -> new Exception("Correntista não encontrado"));
     }
+
+    public void apagarById(Long id)throws Exception{
+        repository.deleteById(id);
+    }
+
 
 }
